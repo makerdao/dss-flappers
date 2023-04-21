f9c6eb6e89c80c300701898cd2c77db17644bc19
oldchili <130549691+oldchili@users.noreply.github.com>
2023-04-21 07:51:36 +0300

    Oracle wrapper tests

---
+++ test/OracleWrapper.t.sol
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.16;
+
+import "forge-std/Test.sol";
+import { OracleWrapper } from "src/OracleWrapper.sol";
+
+contract MockMedianizer {
+    uint256 public price;
+    bool    public has = true;
+
+    function setPrice(uint256 _price) external {
+        price = _price;
+    }
+
+    function setHas(bool _has) external {
+        has = _has;
+    }
+
+    function read() external view returns (bytes32) {
+        require(has);
+        return bytes32(price);
+    }
+}
+
+contract OracleWrapperTest is Test {
+    MockMedianizer public medianizer;
+    OracleWrapper  public oracleWrapper;
+
+    uint256 constant WAD   = 1e18;
+
+    function setUp() public {
+        medianizer = new MockMedianizer();
+        medianizer.setPrice(727 * WAD);
+        medianizer.setHas(true);
+
+        oracleWrapper = new OracleWrapper(address(medianizer), 1800 * WAD);
+    }
+
+    function testRead() public {
+        assertEq(oracleWrapper.read(), bytes32(727 * WAD / 1800)); // 0.40388889e+18
+    }
+
+    function testReadRevert() public {
+        medianizer.setHas(false);
+        vm.expectRevert();
+        oracleWrapper.read();
+    }
+}
