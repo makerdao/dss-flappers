876cc834033cd7832f367498c77e99063d7ab29b
sunbreak <sunbreak1211@proton.me>
2023-07-20 12:54:43 -0300

    Remove unused function in interface

--- src/FlapperUniV2SwapOnly.sol
+++ src/FlapperUniV2SwapOnly.sol
@@ -43,7 +43,6 @@ interface PipLike {
 interface PairLike {
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     function token0() external view returns (address);
-    function mint(address to) external returns (uint256 liquidity);
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
 }
 
